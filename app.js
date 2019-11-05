0295b4abaed99441900e0c3fb9b764703e4a1d4d
Doan Phuc <mrjared16>
2019-11-05 10:10:24 +0700

    add layout and error page

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var handlebars = require('express-handlebars');
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
