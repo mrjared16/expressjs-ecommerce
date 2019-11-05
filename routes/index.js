94d27c675347adf85dacfe836151028ac20d5eb2
Doan Phuc <mrjared16>
2019-11-05 13:49:00 +0700

    change how to list item

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const data = require('./data').array;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  console.log('index');
+  res.render('index', {products: data});
 });
 
 module.exports = router;
