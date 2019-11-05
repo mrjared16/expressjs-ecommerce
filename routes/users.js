f5faf6f18e0885b4ba91840c6651c6a9da990763
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2019-11-05 11:08:53 +0700

    add authentication pages

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+  res.render('user/login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('user/register');
+});
+
+router.get('/forget-password', function(req, res, next) {
+  res.render('user/forget-password');
 });
 
 module.exports = router;
