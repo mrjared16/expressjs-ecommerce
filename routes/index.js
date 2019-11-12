4bb6d0680b65e11609231eb2660e89e055a4f86c
Doan Phuc <mrjared16>
2019-11-12 18:37:29 +0700

    query product from database

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
+
 const data = require('./data');
+const product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  console.log('index');
-  res.render('index', {products: data('list')});
+  product.find().then(result => {
+
+    const query = result.map(item => ({
+      hasBage: (item.sale | item.sale > 0) ? true : false,
+      imgpath: item.img_path[0],
+      name: item.name,
+      price: item.price,
+    }));
+    console.log(query);
+
+    res.render('index', { products: query });
+  });
 });
 
 module.exports = router;
