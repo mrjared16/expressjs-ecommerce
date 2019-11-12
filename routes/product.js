f96c3bc117eb7275da93ea7f9e66348ca7b2cbe0
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2019-11-12 21:26:45 +0700

    fix product/list page

--- routes/product.js
+++ routes/product.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const data = require('./data');
+const product = require('../models/product');
 
 /* GET home page. */
-router.get('/list', function(req, res, next) {
-    res.render('product/list', {products: data('list')});
+router.get('/list', function (req, res, next) {
+    product.aggregate().sample(6).then(result => {
+
+        const query = result.map(item => ({
+            hasBage: (item.sale | item.sale > 0) ? true : false,
+            imgpath: item.img_path[0],
+            name: item.name,
+            price: item.price,
+        }));
+        console.log(query);
+        res.render('product/list', { products: query });
+    });
 });
 
-router.get('/detail', function(req, res, next) {
-    res.render('product/detail', {relate: data('relate'), detail: data('detail')});
+router.get('/detail', function (req, res, next) {
+    res.render('product/detail', { relate: data('relate'), detail: data('detail') });
 });
 
 module.exports = router;
