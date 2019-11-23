16457fb7d5895c1ebebab5054c911f9956be99b0
Phuc Hoang <hvphuc081299@gmail.com>
2019-11-23 15:53:28 +0700

    mvc refactor

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
-const data = require('./data');
-const product = require('../models/product');
+const homeController = require('../controller/homeController');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  product.aggregate().sample(6).then(result => {
-
-    const query = result.map(item => ({
-      hasBage: (item.sale | item.sale > 0) ? true : false,
-      imgpath: item.img_path[0],
-      name: item.name,
-      price: item.price,
-      id: item._id
-    }));
-    console.log(query);
-
-    res.render('index', { products: query });
-  });
-});
+router.get('/', homeController.index);
 
 module.exports = router;
