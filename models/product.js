41416fd8814251b14bf7e10048d85d0eff618997
Phuc Hoang <hvphuc081299@gmail.com>
2019-11-25 13:46:41 +0700

    add image products

--- models/product.js
+++ models/product.js
@@ -1,21 +1,47 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
+    // name: String,
+    // brand: String,
+    // type: String,
+    // categories: [String],
+    // price: Number,
+    // quantity: Number,
+    // sale: Number,
+    // img_path: [String],
+    // review: [{
+    //     username: String,
+    //     comment: String,
+    //     date: Date
+    // }],
+    // description: String,
+    // rating: Number
+
     name: String,
     brand: String,
-    type: String,
-    categories: [String],
+    option:{
+        color: [String],
+        size: [String]
+    },
+    tag: [String],
     price: Number,
     quantity: Number,
     sale: Number,
-    img_path: [String],
+    assert:{
+        path: String,
+        img: [String]
+    },
     review: [{
         username: String,
         comment: String,
         date: Date
     }],
     description: String,
-    rating: Number
+    rating: Number,
+    group: String,
+    category: String,
+    gender: String,
+	view: Number
 });
 
 module.exports = mongoose.model('product', ProductSchema, 'product');
