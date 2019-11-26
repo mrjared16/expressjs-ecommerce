d112e9e0a5bfc2eb13180aee711c0613a0b6f1c5
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2019-11-26 17:37:58 +0700

    clean the old code

--- models/product.js
+++ models/product.js
@@ -1,22 +1,6 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    // name: String,
-    // brand: String,
-    // type: String,
-    // categories: [String],
-    // price: Number,
-    // quantity: Number,
-    // sale: Number,
-    // img_path: [String],
-    // review: [{
-    //     username: String,
-    //     comment: String,
-    //     date: Date
-    // }],
-    // description: String,
-    // rating: Number
-
     name: String,
     brand: String,
     option:{
