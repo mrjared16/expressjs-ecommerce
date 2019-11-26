1c123b0c94f82e491a17d85944171ddda4e7f0fc
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2019-11-27 01:14:23 +0700

    routing for product page

--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var productController = require('../controller/productController');
 
 router.get('/list', productController.index);
+router.get('/list/:page', productController.index);
 
 router.get('/:id', productController.detail)
 
