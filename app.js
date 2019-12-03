33bf1c303f0c1fa91fbac12bf8255dca90a18a0a
Doan Phuc <mrjared16>
2019-12-03 21:14:30 +0700

    add passport and set user to local

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
+var session = require('express-session');
+var passport = require('passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productRouter = require('./routes/product');
 var checkoutRouter = require('./routes/checkout');
 var dashboardRouter = require('./routes/dashboard');
-
+const userService = require('./models/userService');
 var app = express();
 
 //const mongoDB = 'mongodb://localhost/ecommerce';
@@ -22,7 +24,7 @@ mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopolo
 
 var handlebars = require('express-handlebars');
 app.engine('hbs', handlebars({
-  extname: '.hbs', 
+  extname: '.hbs',
   helpers: require('./views/helpers') //only need this
 }));
 
@@ -30,12 +32,61 @@ app.engine('hbs', handlebars({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+// Using LocalStrategy with passport
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    userService.getUserByUsername(username, function (err, user) {
+      console.log('checking');
+      if (err) err;
+      if (!user) {
+        return done(null, false, { message: 'Unknown User' });
+      }
+      console.log('checking password');
+      userService.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) throw err;
+        if (isMatch) {
+          console.log('login');
+          console.log(user);
+          return done(null, user);
+        } else {
+          console.log('not login');
+          return done(null, false, { message: 'Invalid password' });
+        }
+      });
+    });
+  }
+));
+
+passport.serializeUser(function (user, done) {
+  console.log("get");
+  done(null, user.username);
+});
+
+passport.deserializeUser(function (username, done) {
+  console.log("get user");
+  userService.getUserByUsername(username, function (err, user) {
+    console.log("get done");
+    done(err, user);
+  });
+});
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static', express.static(path.join(__dirname, 'public')));
-
+app.use(function (req, res, next) {
+  if (req.user) res.locals.user = req.user; next();
+});
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/product', productRouter);
