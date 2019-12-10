dcd8367b58c11b4296f88fd1c3beb3adccb6c579
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2019-12-10 17:51:44 +0700

    restrict userSerivce if not approriate access

--- controller/userController.js
+++ controller/userController.js
@@ -2,14 +2,22 @@ const userService = require('../models/userService');
 const passport = require('passport');
 
 exports.getLogin = (req, res) => {
-    
+    if (isAuthenticated(req, res)) {
+        res.redirect('/dashboard');
+        return;
+    }
     res.render('user/login');
 }
 
 exports.postLogin = (req, res, next) => {
+    if (isAuthenticated(req, res)) {
+        res.redirect('/dashboard');
+        return;
+    }
+
     passport.authenticate('local', function (err, user, message) {
-        if (err) { 
-            return next(err); 
+        if (err) {
+            return next(err);
         }
         if (!user) {
             return res.render('user/login', { alert: { type: 'danger', message: `${message}` } });
@@ -25,10 +33,20 @@ exports.postLogin = (req, res, next) => {
 
 
 exports.getRegister = (req, res) => {
+    if (isAuthenticated(req, res)) {
+        res.redirect('/dashboard');
+        return;
+    }
+
     res.render('user/register');
 }
 
 exports.postRegister = async (req, res) => {
+    if (isAuthenticated(req, res)) {
+        res.redirect('/dashboard');
+        return;
+    }
+
     const validate = await userService.registerValidate(req.body);
     if (validate.result) {
         await userService.createUser(req.body);
@@ -41,28 +59,59 @@ exports.postRegister = async (req, res) => {
 
 
 exports.getForgetPass = (req, res) => {
+    if (isAuthenticated(req, res)) {
+        res.redirect('/dashboard');
+        return;
+    }
+
     res.render('user/forget-password');
 }
 
 //TODO
 exports.postForgetPass = (req, res) => {
+    if (isAuthenticated(req, res)) {
+        res.redirect('/dashboard');
+        return;
+    }
     userService.forgetPassword(req.body);
     res.render('user/register', { alert: { type: 'success', message: 'Đã gửi đến email của bạn' } });;
 }
 
 exports.logout = (req, res) => {
-    req.logout();
+    if (isAuthenticated(req, res)) {
+        req.logout();
+    }
     res.redirect('/');
 }
 
 exports.history = (req, res) => {
+    if (!isAuthenticated(req, res)) {
+        res.redirect('/');
+        return;
+    }
+
     res.render('dashboard/history');
 };
 
 exports.address = (req, res) => {
+    if (!isAuthenticated(req, res)) {
+        res.redirect('/');
+        return;
+    }
+
     res.render('dashboard/address');
 };
 
 exports.profile = (req, res) => {
+    if (!isAuthenticated(req, res)) {
+        res.redirect('/');
+        return;
+    }
+
     res.render('dashboard/profile');
 };
+
+const isAuthenticated = (req, res) => {
+    return (req.user) ? true : false;
+}
+
