ddaf1e61d9f70bbce8e1cf169767ac6a1f35f170
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2019-12-10 21:10:23 +0700

    form listening

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,11 @@
   // Preloader
   $(window).on('load', function () {
     $('#preloader').fadeOut('slow', function () { $(this).remove(); });
+    $('.filter-form').change(
+      () => {
+        $('#queryForm').submit();
+      }
+    );
   });
 
   // e-commerce touchspin
