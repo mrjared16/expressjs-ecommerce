55207376aaadf69466278cac9f2dbe8d45dc68f9
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2019-12-10 21:09:30 +0700

    change Controller Name

--- controller/productController.js
+++ controller/productController.js
@@ -1,12 +1,15 @@
-const productService = require('../models/productService') 
+const productService = require('../models/productService')
 
 
-exports.index = async (req, res) => {
-    const {products, pageOptions} = await productService.queryIndex(req, res);
-    res.render('product/list', { products: products, pageOptions: pageOptions} );
+exports.getList = async (req, res) => {
+    if (req.query) {
+        res.locals.query = req.query;
+    }
+    const { products, pageOptions } = await productService.queryIndex(req, res);
+    res.render('product/list', { products: products, pageOptions: pageOptions });
 }
 
-exports.detail = async (req, res) => {
+exports.getDetail = async (req, res) => {
     const queryDetail = await productService.queryDetail(req, res);
     res.render('product/detail', {
         relate: null,
