cd2150e539ef1e7caced2adddc7dc92050c7382e
Doan Phuc <mrjared16>
2019-12-10 16:50:10 +0700

    fix decentralize multiple time

--- app.js
+++ app.js
@@ -1,92 +1,62 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
-var session = require('express-session');
-var passport = require('passport');
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./config/passport');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var productRouter = require('./routes/product');
-var checkoutRouter = require('./routes/checkout');
-var dashboardRouter = require('./routes/dashboard');
-const userService = require('./models/userService');
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const productRouter = require('./routes/product');
+const checkoutRouter = require('./routes/checkout');
+const dashboardRouter = require('./routes/dashboard');
 
-//const mongoDB = 'mongodb://localhost/ecommerce';
+
+const app = express();
+
+// static file first
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// config mongoose
 mongoose.set('useFindAndModify', false);
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  //const mongoDB = 'mongodb://localhost/ecommerce';
   console.log('connected database');
 });
 
-var handlebars = require('express-handlebars');
+// config handle bars
+const handlebars = require('express-handlebars');
 app.engine('hbs', handlebars({
   extname: '.hbs',
-  helpers: require('./views/helpers') //only need this
+  helpers: require('./views/helpers')
 }));
 
-// view engine setup
+// config view engine 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// config session
 app.use(session({
   secret: 'secret',
   saveUninitialized: true,
   resave: true
 }));
-// Using LocalStrategy with passport
-var LocalStrategy = require('passport-local').Strategy;
-passport.use(new LocalStrategy(
-  function (username, password, done) {
-    userService.getUserByUsername(username, function (err, user) {
-      console.log('checking');
-      if (err) err;
-      if (!user) {
-        return done(null, false, { message: 'Unknown User' });
-      }
-      console.log('checking password');
-      userService.comparePassword(password, user.password, function (err, isMatch) {
-        if (err) throw err;
-        if (isMatch) {
-          console.log('login');
-          console.log(user);
-          return done(null, user);
-        } else {
-          console.log('not login');
-          return done(null, false, { message: 'Invalid password' });
-        }
-      });
-    });
-  }
-));
-
-passport.serializeUser(function (user, done) {
-  console.log("get");
-  done(null, user.username);
-});
-
-passport.deserializeUser(function (username, done) {
-  console.log("get user");
-  userService.getUserByUsername(username, function (err, user) {
-    console.log("get done");
-    done(err, user);
-  });
-});
 
 // Passport init
 app.use(passport.initialize());
 app.use(passport.session());
+// config passport
+passportConfig(passport, app);
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/static', express.static(path.join(__dirname, 'public')));
-app.use(function (req, res, next) {
-  if (req.user) res.locals.user = req.user; next();
-});
+
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/product', productRouter);
