83e7d328f5a72dd468f7b9e44a9c8facf172015d
Phuc Hoang <hvphuc081299@gmail.com>
2020-01-01 17:02:30 +0700

    get user info and pass view

--- controller/cartController.js
+++ controller/cartController.js
@@ -7,7 +7,7 @@ exports.postAddItemInCart = async (req, res) => {
     req.session.cart = [];
   }
   await cartService.addItemInCart(req.session.cart, req.params.id);
-  let items = await cartService.itemsInCart(req.session.cart);
+  let items = await cartService.getItemsInCart(req.session.cart);
   if (req.session.username != null) {
     await cartService.setItemsInOrderUser(req.session.cart, req.session.username);
   }
@@ -17,12 +17,34 @@ exports.postAddItemInCart = async (req, res) => {
 }
 
 exports.postDeleteItemInCart = async (req, res) => {
-  await cartService.deleteItemInCart(req.session.cart, req.params.id);
-  let items = await cartService.itemsInCart(req.session.cart);
+  console.log(req.session.cart);
+  req.session.cart = await cartService.deleteItemInCart(req.session.cart, req.params.id);
+  console.log(req.session.cart);
+  let items = await cartService.getItemsInCart(req.session.cart);
   if (req.session.username != null) {
     await cartService.setItemsInOrderUser(req.session.cart, req.session.username);
   }
   req.app.locals.itemsInMyCart = items;
+  console.log(req.app.locals.itemsInMyCart);
   req.app.locals.totalPrice = await cartService.totalPriceInCart(req.session.cart);
+  console.log(req.session.cart);
   res.send(items);
+}
+
+exports.getPayment = async (req, res) => {
+  console.log("1");
+  if (!isAuthenticated(req, res)) {
+    console.log("2.1");
+    res.render('checkout/payment');
+  } else {
+    console.log("2.2");
+    const userInfo = await cartService.getUserInfoToPayment(req.user.id);
+    console.log("3");
+    console.log(userInfo);
+    res.render('checkout/payment', {fullname: userInfo.fullname, phone: userInfo.phone});
+  }
+}
+
+const isAuthenticated = (req, res) => {
+    return (req.user) ? true : false;
 }
