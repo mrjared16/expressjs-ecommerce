32e893fdb33b0666c20a0cb1f270625fc6765ec3
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2020-01-03 12:05:34 +0700

    refactor cart

--- controller/cartController.js
+++ controller/cartController.js
@@ -1,49 +1,37 @@
 const cartService = require('../models/cartService');
-const express = require('express');
-const app = express();
+const userService = require('../models/userService');
+
 
 exports.postAddItemInCart = async (req, res) => {
   if (req.session.cart == null) {
     req.session.cart = [];
   }
-  await cartService.addItemInCart(req.session.cart, req.params.id);
-  let items = await cartService.getItemsInCart(req.session.cart);
-  if (isAuthenticated(req, res)) {
-    await cartService.setItemsInOrderUser(req.session.cart, req.user.id);
-  }
-  req.app.locals.itemsInMyCart = items;
-  req.app.locals.totalPrice = await cartService.totalPriceInCart(req.session.cart);
+  const productId = req.params.id;
+  req.session.cart = cartService.addItemToCart(req.session.cart, productId);
+  const items = await cartService.updateCart(req, res);
   res.send(items);
 }
 
 exports.postDeleteItemInCart = async (req, res) => {
-  console.log(req.session.cart);
-  req.session.cart = await cartService.deleteItemInCart(req.session.cart, req.params.id);
-  console.log(req.session.cart);
-  let items = await cartService.getItemsInCart(req.session.cart);
-  if (isAuthenticated(req, res)) {
-    await cartService.setItemsInOrderUser(req.session.cart, req.user.id);
-  }
-  req.app.locals.itemsInMyCart = items;
-  console.log(req.app.locals.itemsInMyCart);
-  req.app.locals.totalPrice = await cartService.totalPriceInCart(req.session.cart);
-  console.log(req.session.cart);
+  const productId = req.params.id;
+
+  req.session.cart = cartService.deleteItemInLocalCart(req.session.cart, productId);
+
+  const items = await cartService.updateCart(req, res);
   res.send(items);
 }
 
 exports.getPayment = async (req, res) => {
-  if (!isAuthenticated(req, res)) {
-    res.render('checkout/payment');
-  } else {
-    const userInfo = await cartService.getUserInfoToPayment(req.user.id);
-    res.render('checkout/payment', {fullname: userInfo.fullname, phone: userInfo.phone});
+  let viewModel = {};
+
+  if (userService.isAuthenticated(req, res)) {
+    const userInfo = await cartService.getUserInfoToPayment(req.user._id);
+    viewModel = { fullname: userInfo.fullname, phone: userInfo.phone };
   }
+
+  res.render('checkout/payment', viewModel);
 }
 
 exports.postPayment = async (req, res) => {
 
-}
-
-const isAuthenticated = (req, res) => {
-    return (req.user) ? true : false;
 }
