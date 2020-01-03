840f595ad86ae60063190928f0bf0130f825026f
Doan Phuc <mrjared16>
2020-01-03 12:05:34 +0700

    refactor cart

--- models/cartService.js
+++ models/cartService.js
@@ -1,101 +1,182 @@
-const User = require('./user');
-const Product = require('./product')
+const { User } = require('./userModel');
+const { Product } = require('./productModel');
+const { Cart } = require('./cartModel');
 const userService = require('../models/userService');
+const mongoose = require('mongoose');
 
-exports.addItemInCart = async (myCart, itemId) => {
-  var itemInfo = {id: itemId, quantity: "1"}
-  await myCart.forEach(async (item, index) => {
-    if (item.id == itemId) {
-      item.quantity = await new Promise((resolve, reject) => {
-        const numberOfItem = parseInt(item.quantity) + 1;
-        resolve(numberOfItem + "")
-      });
-      itemInfo.quantity = item.quantity;
-    }
-  })
-  if (itemInfo.quantity == 1) {
-    await myCart.push(itemInfo);
+
+exports.updateCart = async (req, res) => {
+  // sync session to database
+  if (userService.isAuthenticated(req, res)) {
+    req.session.cart = await exports.syncLocalCartToDatabase(req.session.cart, req.user);
   }
+
+  // get cart item view model
+  let { totalPrice, items } = await exports.getItemsDetailInCart(req.session.cart);
+  // console.log(totalPrice, items);
+  req.app.locals.itemsInMyCart = items;
+  req.app.locals.totalPrice = totalPrice;
+  return items;
 }
 
-exports.getItemsInCart = async (myCart) => {
-  var itemsInfo = [];
-  await Promise.all(myCart.map(async (item) => {
-    let temp  = await Product.findOne({_id: item.id});
-    let formatData = {
-        id: temp.id,
-        name: temp.name,
-        price: temp.price,
-        img: temp.assert.path + temp.assert.img[0],
-        quantity: item.quantity
-    };
-    itemsInfo.push(formatData);
-  }));
-  return itemsInfo;
+exports.addItemToCart = (myCart, itemId) => {
+  let found = false;
+  myCart.forEach((item, index) => {
+    if (item.product == itemId) {
+      myCart[index].quantity++;
+      found = true;
+    }
+  });
+
+  if (!found) {
+    const newItem = {
+      product: itemId,
+      quantity: 1
+    }
+    myCart.push(newItem)
+  }
+
+  return myCart;
 }
 
-exports.deleteItemInCart = async (myCart, itemId) => {
-  await Promise.all(myCart.map((item, index) => {
-    if (item.id == itemId) {
+exports.deleteItemInLocalCart = (myCart, itemId) => {
+  myCart.forEach((item, index) => {
+    if (item.product == itemId) {
       myCart.splice(index, 1);
     }
-  }))
-  // return myCart.filter((item) => {
-  //   return item.id != itemId;
-  // })
+  });
+  return myCart;
 }
 
-exports.totalPriceInCart = async (myCart) => {
-  let total = 0;
-  await Promise.all(myCart.map(async (item) => {
-    let temp  = await Product.findOne({_id: item.id});
-    let formatData = {
-        id: temp.id,
-        name: temp.name,
-        price: temp.price,
-        img: temp.assert.path + temp.assert.img[0],
-        quantity: item.quantity
-    };
-    total = total + parseInt(temp.price) * parseInt(item.quantity);
-  }));
-  if (total == 0) {
-    return null;
+exports.getCartItemViewModel = (product, { quantity }) => {
+  return {
+    id: product.id,
+    name: product.name,
+    price: product.price,
+    img: product.assert.img[0],
+    quantity
   }
-  return total;
 }
 
-async function getItemInfoFromProductModel(myCart) {
-  let items = [];
-  let total = 0;
-  await Promise.all(myCart.map(async (item) => {
-    let temp  = await Product.findOne({_id: item.id});
-    let itemInfo = {
-      item_id: temp.id,
-      quantity: item.quantity,
-      price: temp.price
-    };
-    total = total + parseInt(temp.price) * parseInt(item.quantity);
-    items.push(itemInfo);
-  }));
-  return {items: items, totalPrice: total};
+exports.getItemsDetailInCart = async (myCart) => {
+  let totalPrice = 0;
+  if (myCart == null) {
+    myCart = [];
+  }
+  const items = await Promise.all(
+    myCart.map((item) => new Promise(
+      async (resolve, reject) => {
+        const product = await Product.findOne({ _id: item.product });
+        totalPrice += product.price * item.quantity;
+        resolve(exports.getCartItemViewModel(product, item));
+      }
+    ))
+  );
+  return { items, totalPrice };
 }
 
-async function setOrderFieldUser(userOrder, myCart) {
-  let itemsAndTotalPrice = await getItemInfoFromProductModel(myCart);
-  let order = {
-    status: "Chưa giao",
-    data: new Date(),
-    items: itemsAndTotalPrice.items,
-    total_price: itemsAndTotalPrice.totalPrice
-  };
-  userOrder.order.push(order);
-  await userOrder.save();
+// exports.getItemsInCart = async (myCart) => {
+//   let totalPrice = 0;
+//   if (myCart == null) {
+//     myCart = [];
+//   }
+//   const items = await Promise.all(
+//     myCart.map((item) => new Promise(
+//       async (resolve, reject) => {
+//         const unit_price = (await Product.findOne({ _id: item.product })).price;
+//         totalPrice = totalPrice + unit_price * item.quantity;
+//         resolve({
+//           product: item.product,
+//           quantity: item.quantity,
+//         });
+//       }
+//     ))
+//   );
+//   return { items, totalPrice };
+// }
+
+exports.syncLocalCartToDatabase = async (myCart, { _id }) => {
+  // find cart in database
+  const cart = await Cart.findOne({ user: _id });
+
+  // local cart null
+  if (myCart == null)
+    myCart = [];
+
+  // database cart null
+  if (!cart) {
+    cart = await new Cart({
+      user: _id,
+    });
+  }
+
+  cart.items = myCart;
+  await cart.save();
+  return myCart;
 }
 
-exports.setItemsInOrderUser = async (myCart, userId) => {
-  const userOrder = await User.findOne({_id: userId});
-  await setOrderFieldUser(userOrder, myCart);
+exports.insertLocalToDatabase = async (req) => {
+  // found => save new items
+  let cart = await Cart.findOne({ user: req.user._id });
+
+  // local cart null
+  if (req.session.cart == null) {
+    req.session.cart = [];
+  }
+
+  // database cart null
+  if (!cart) {
+    cart = await new Cart({
+      user: req.user._id,
+      items: []
+    });
+  }
+
+  // merge
+  req.session.cart.forEach(local => {
+    let found = false;
+    let i;
+    for (i = 0; i < cart.items.length; i++) {
+      if (cart.items[i].product.toString() == local.product) {
+        cart.items[i].quantity += local.quantity;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      // console.log('add from local cart: ', local);
+      cart.items.push(local);
+    } else {
+      // console.log('update quantity: ', cart.items[i]);
+    }
+  })
+
+  // save
+  cart = await cart.save();
+
+  // new local cart
+  req.session.cart = cart.items;
+
+  // update view
+  let { totalPrice, items } = await exports.getItemsDetailInCart(req.session.cart);
+  // console.log(totalPrice, items);
+  req.app.locals.itemsInMyCart = items;
+  req.app.locals.totalPrice = totalPrice;
 }
+
+// async function setOrderFieldUser(userOrder, myCart) {
+//   let itemsAndTotalPrice = await getItemInfoFromProductModel(myCart);
+//   let order = {
+//     status: "Chưa giao",
+//     data: new Date(),
+//     items: itemsAndTotalPrice.items,
+//     total_price: itemsAndTotalPrice.totalPrice
+//   };
+//   //userOrder.order.push(order);
+//   await userOrder.save();
+// }
+
+
 
 exports.getUserInfoToPayment = async (userId) => {
   return await userService.getUserInfo(userId);
