55258bb2f3d1b7d57d641510e532a62b184dea5a
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2020-01-07 17:53:24 +0700

    add alert partial view

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    console.log('custom middleware!');
+    // console.log('custom middleware!');
     if (req.isAuthenticated && req.isAuthenticated()) {
         res.locals.user = req.user;
         // console.log(req.user);
