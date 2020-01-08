985a8aededaff11f801778c9e2d0f3e7320a1800
Đoàn Phúc <doannguyentanphuc99@gmail.com>
2020-01-08 09:51:35 +0700

    add review API

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,9 +1,10 @@
 const commentService = require('../models/commentService');
+const Util = require('../util');
 
 //add new review to product
 exports.addNewReview = async (req, res) => {
     // (!req.reviewBody || 0 === reviewBody.length);
-    if (!req.isAuthenticated() || (!req.body.name || req.body.name.length === 0)) {
+    if (!req.isAuthenticated() && (!req.body.name || req.body.name.length === 0)) {
         req.flash('alert', 'danger');
         req.flash('alert', 'Bạn phải nhập tên để đánh giá');
         res.redirect('/product/' + req.body.productId + "#reviews");
@@ -22,4 +23,25 @@ exports.addNewReview = async (req, res) => {
     });
     // comment successful
     res.redirect('/product/' + req.body.productId + "#reviews");
+}
+
+exports.getReviews = async (req, res, next) => {
+    const [id, page, limit] = [req.params.id, ...[req.query.page, req.query.limit].map(x => parseInt(x))];
+
+    const { review } = await commentService.getReviews(id, page, limit);
+
+    if (!review){
+        console.log('get reviews error');
+        res.send(new Error('error'));
+    }
+    const result = review.map(_review => {
+        return {
+            body: _review.body,
+            createdAt: Util.getDateFormat(_review.createdAt),
+            author: _review.author,
+            name: (_review.author) ? _review.author.name : _review.name,
+            avatar: (_review.author) ? _review.author.avatar : ''
+        };
+    });
+    res.send(result);
 }
